52ddb87d4a25a0faa20e4d355d1e0fad5656037c
iBessonob <bessonov3145@gmail.com>
2020-02-15 17:19:41 -0800

    Fixed eye.py

--- eye.py
+++ eye.py
@@ -1,32 +1,11 @@
 import cv2
-import os, io
-from google.cloud import vision
+import os
 
-api_key = "5f8c28f5ffb473cc5fec829109919c4275ebb304"
+#api_key = "5f8c28f5ffb473cc5fec829109919c4275ebb304"
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'cloud-vision.json'
-#cap = cv2.VideoCapture(0)
 
-def detect_faces(path):
-    """Detects eye positions in an image."""
-    from google.cloud import vision
-    import io
-    client = vision.ImageAnnotatorClient()
-
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
-
-    image = vision.types.Image(content=content)
-
-    response = client.face_detection(image=image)
-    faces = response.face_annotations
-
-    # Names of likelihood from google.cloud.vision.enums
-    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
-                       'LIKELY', 'VERY_LIKELY')
-    
-    #stores tuples (x,y,z) float coords of eye attribute positions
-    eye_positions = []
-    '''
+#stores tuples (x,y,z) float coords of eye attribute positions
+'''
     See array indexes for specific attr below vvvvvvvv
     0: Left eye
     1: Right eye
@@ -36,23 +15,63 @@ def detect_faces(path):
     5: Right of Right Eyebrow
     6: Midpoint of Eyes
     '''
-    _ = 0
-    for f in faces:
-        for l in f.landmarks:
-            eye_positions.append((l.position.x,l.position.y,l.position.z))
-            if _ == 6: break
-            _+=1
-    ''' 
-        print('anger: {}'.format(likelihood_name[face.anger_likelihood]))
-        print('joy: {}'.format(likelihood_name[face.joy_likelihood]))
-        print('surprise: {}'.format(likelihood_name[face.surprise_likelihood]))
+eye_positions = []
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in face.bounding_poly.vertices])
+def detect_eyes():
+    #Detects eye positions in an image.
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
 
-        print('face bounds: {}'.format(','.join(vertices)))
-    for _ in eye_positions:
-        print(_)'''
+    cap = cv2.VideoCapture(0)
+
+    ret,frame1 = cap.read()
+
+    #ret,frame2 = cap.read()
+
+    while cap.isOpened():
+        gray = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+        _ = 0
+
+        image = vision.types.Image(content=cv2.imencode('.jpg', frame1)[1].tostring())
+
+        response = client.face_detection(image=image)
+        faces = response.face_annotations
+        print(faces)
+
+        for f in faces:
+            for l in f.landmarks:
+                eye_positions.append((l.position.x,l.position.y,l.position.z))
+                if _ == 6: break
+                _+=1
+        
+        print("Positions List: ",eye_positions)
+        for _ in eye_positions:
+            # Center coordinates 
+            center_coordinates = (int(_[0]),int(_[1])) 
+            print("CENTER COORDS: ",center_coordinates)
+
+            # Radius of circle 
+            radius = 2
+            
+            # Blue color in BGR 
+            color = (0, 0, 255) 
+            
+            # Line thickness of 2 px 
+            thickness = 2
+            
+            # Using cv2.circle() method 
+            # Draw a circle with blue line borders of thickness of 2 px 
+            image = cv2.circle(frame1, center_coordinates, radius, color, thickness) 
+            #cv2.circle(frame1,(_[0],_[1]),2,(0, 0, 255),2)
+            print(_)
+        print('---------------------------')
+        
+        cv2.imshow('feed',frame1)
+        ret,frame1 = cap.read()
+
+        if cv2.waitKey(40) == 27:
+            break
 
     if response.error.message:
         raise Exception(
@@ -61,6 +80,6 @@ def detect_faces(path):
                 response.error.message))
 
 if __name__ == '__main__':
-    detect_faces("driver.jpg")
+    detect_eyes() #"driver.jpg"
 
 print("succesful???")
